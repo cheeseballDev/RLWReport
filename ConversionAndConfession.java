4e71de3fb7593e4225390c6652d99482b9ba524b
cheeseballDev <cheeseballbusinessemail@gmail.com>
2024-11-04 00:17:09 +0800

    realer

--- ConversionAndConfession.java
+++ ConversionAndConfession.java
@@ -1,25 +1,70 @@
 package RLWReport;
 
+import java.util.Scanner;
+
 public class ConversionAndConfession implements Runnable {
     private char[] dialogue;
     void start() {
+        Scanner userInput = new Scanner(System.in);
         clearScreen();
-        String string = "Rizal Life and Works GROUP 5 - Confession and The Demand of Conversion";
-        this.dialogue = string.toCharArray();
+        this.dialogue = "Rizal Life and Works - The Demand of Conversion and Confession\nPresented by Group 5 (Bancual, Contreras, Espartero, Punzal, Tuprio, Villar)\nPress ENTER to continue\n".toCharArray();
         showDialogue(dialogue);
+        userInput.nextLine();
+        goToMainMenu(userInput);
+    }
+
+    private void goToMainMenu(Scanner userInput) {
+        clearScreen();
+        while(true) {
+            this.dialogue = "What do you want to know about? (PRESS THE CORRESPONDING NUMBER YOU WANT TO SELECT AND HIT ENTER)\n1. Conversion\n2. Confession".toCharArray();
+            showDialogue(dialogue);
+            String userChoice = userInput.nextLine();
+            switch(userChoice) {
+                case "1":
+                    goToConversion(userInput);
+                    break;
+                case "2":
+                    goToConfession(userInput);
+                    break;
+                default:
+                    showError("Invalid number! Please try again.");
+            }
+        }
+    }
+
+    private void goToConversion(Scanner userInput) {
+        this.dialogue = "conversion".toCharArray();
+    }
+
+    private void goToConfession(Scanner userInput) {
+        this.dialogue = "confession".toCharArray();
+    }
+
+    /*
+     * MISC
+     */
+
+    private void showError(String errorName) {
+        System.out.println("Please enter a valid number!");
+        char[] characters = {'.', ' ', '.', ' ', '.', ' ', '.'};
+        for(int i = 0; i < characters.length; i++) {
+            System.out.print(characters[i]);
+            run(350);
+        }
+        clearScreen();
     }
 
     private void showDialogue(char[] dialogue) {
         char[] characters = dialogue;
         for(int i = 0; i < characters.length; i++) {
             System.out.print(characters[i]);
-            run();
+            run(50);
         }
     }
 
-    public void run() {
+    public void run(int milliseconds) {
         try {
-            Thread.sleep(50);
+            Thread.sleep(milliseconds);
         } catch (Exception e) {
             e.printStackTrace();
         }
